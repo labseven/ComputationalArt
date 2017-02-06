861a5a4b654703e55b03d8b1f49f75e2d6fd6af7
Adam Yesvotny <adam.novotny55@gmail.com>
2017-02-06 14:00:39 -0500

    first pass evaluate random function

--- recursive_art.py
+++ recursive_art.py
@@ -21,24 +21,29 @@ from PIL import Image
 #     """
 #     # TODO: implement this
 #     pass
-#
-#
-# def evaluate_random_function(f, x, y):
-#     """ Evaluate the random function f with inputs x,y
-#         Representation of the function f is defined in the assignment writeup
-#
-#         f: the function to evaluate
-#         x: the value of x to be used to evaluate the function
-#         y: the value of y to be used to evaluate the function
-#         returns: the function value
-#
-#         >>> evaluate_random_function(["x"],-0.5, 0.75)
-#         -0.5
-#         >>> evaluate_random_function(["y"],0.1,0.02)
-#         0.02
-#     """
-#     # TODO: implement this
-#     pass
+
+
+def evaluate_random_function(f, x, y):
+    """ Evaluate the random function f with inputs x,y
+        Representation of the function f is defined in the assignment writeup
+
+        f: the function to evaluate
+        x: the value of x to be used to evaluate the function
+        y: the value of y to be used to evaluate the function
+        returns: the function value
+
+        >>> evaluate_random_function(["x"],-0.5, 0.75)
+        -0.5
+        >>> evaluate_random_function(["y"],0.1,0.02)
+        0.02
+    """
+
+    if(f[0] == 'x'):
+        return x
+    if(f[0] == 'y'):
+        return y
+    else:
+        raise(ValueError("Function not defined."))
 
 
 def remap_interval(val,
