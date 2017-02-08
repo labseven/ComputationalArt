8b4e87b2159c6d5045fe52e874c9573589f281e1
Adam Yesvotny <adam.novotny55@gmail.com>
2017-02-08 17:07:49 -0500

    first tree test

---
+++ treeTest.py
@@ -0,0 +1,33 @@
+class Tree:
+    def __init__(self, cargo, left=None, right=None):
+        self.cargo = cargo
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.cargo)
+
+
+def total(tree):
+    if tree is None: return 0
+    return total(tree.left) + total(tree.right) + tree.cargo
+
+
+class TreeFunctions:
+    def __init__(self, value, left=None, right=None):
+        self.cargo = cargo
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return
+
+
+l = Tree(1)
+r = Tree(2)
+t = Tree(3, l, r)
+
+print(l)
+print(r)
+print(t)
+print(total(t))
